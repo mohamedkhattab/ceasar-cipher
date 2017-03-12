6e742b079a14b0d2dd49b3f96cd3e4799fe0e848
Felix Mendelssohn <mendelssohn@protonmail.com>
2017-03-12 21:14:48 +0200

    added Static server

--- main.go
+++ main.go
@@ -1,20 +1,23 @@
 package main
 
 import (
-	"fmt"
-
-	bf "github.com/mohamedkhattab/ceasar-cipher/bruteforce"
-	cc "github.com/mohamedkhattab/ceasar-cipher/ceasarcipher"
+	"html/template"
+	"net/http"
 )
 
-func main() {
-	cipher := cc.MultiCeasar{[][2]int32{
-		[2]int32{0, 4},
-	}}
-	enc := cipher.Encrypt("school sucks")
-	fmt.Println(enc)
+func index(w http.ResponseWriter, r *http.Request) {
+	templates := template.Must(template.ParseFiles("templates/index.html"))
+	templates.ExecuteTemplate(w, "layout", nil)
+}
 
-	cracker := bf.BruteForceMulti{}
-	key, text := cracker.Crack(enc)
-	fmt.Printf("Key: %d, message: %s", key, text)
+func main() {
+	mux := http.NewServeMux()
+	files := http.FileServer(http.Dir("/public"))
+	mux.Handle("/public/", files)
+	mux.HandleFunc("/", index)
+	server := &http.Server{
+		Addr:    "0.0.0.0:8080",
+		Handler: mux,
+	}
+	server.ListenAndServe()
 }
